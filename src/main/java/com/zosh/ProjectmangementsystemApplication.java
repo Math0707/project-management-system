611d6536fa5b21823e3b1ce17ad4c8a0cf523cf9
Matheus Teixeira <mteixeirafrancelino2@gmail.com>
2026-03-02 07:41:13 -0300

    Primeiro commit

---
+++ src/main/java/com/zosh/ProjectmangementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.zosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmangementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmangementsystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/zosh/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.zosh.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "secret-key";
+    public static final String JWT_HEADER="AUthorization";
+}
